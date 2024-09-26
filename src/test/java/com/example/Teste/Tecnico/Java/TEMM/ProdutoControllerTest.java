0b0cd7d13e6df3f10bb93de1fef91a68fe5f842c
Koutsoft <souza.marcondes@outlook.com>
2024-09-26 15:21:44 -0300

    adicao dos testes ProdutoController

--- src/test/java/com/example/Teste/Tecnico/Java/TEMM/ProdutoControllerTest.java
+++ src/test/java/com/example/Teste/Tecnico/Java/TEMM/ProdutoControllerTest.java
@@ -1,17 +1,17 @@
 package com.example.Teste.Tecnico.Java.TEMM;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.Teste.Tecnico.Java.TEMM.domain.Controller.ProdutoController;
+import com.example.Teste.Tecnico.Java.TEMM.domain.Entity.Categoria;
 import com.example.Teste.Tecnico.Java.TEMM.domain.Entity.Produto;
-import com.example.Teste.Tecnico.Java.TEMM.domain.repository.ProdutoRepository;
 import com.example.Teste.Tecnico.Java.TEMM.domain.service.ProdutoService;
 
 
@@ -20,30 +20,73 @@ import com.example.Teste.Tecnico.Java.TEMM.domain.service.ProdutoService;
 
 public class ProdutoControllerTest {
 
-    @Mock 
-    private ProdutoRepository produtoRepository;
-    private ProdutoService service;
+
     @InjectMocks
     ProdutoController controller;
+
+    @Mock
+    ProdutoService service;
+
+    @BeforeEach
+    public void setup(){
+    }
    
 
     @Test
     public void cadastrarProduto(){
-         Mockito.when(service.salvarNovoProduto(this.ProdutoTest())).thenReturn(new Produto());
-        
+      controller.cadastrarProduto(this.prod());
+      
+    }
+
+    @Test
+    public void listarProdutosTest()throws Exception{
+        controller.listarProdutos();
+    }
+
+    @Test
+    public void deletarProduto(){
+        controller.deletarProduto(1L);
+    }
+
+    @Test
+    public void updateProduto(){
+        controller.updateProduto(1L,this.prod());
+    }
+
+    @Test
+    public void listarTodosOsProdutosPorCategoria(){
+        controller.listarTodosOsProdutosPorCategoria(1L);
     }
 
 
-    private Produto ProdutoTest(){
+
+    private Produto prod(){
         Produto prod = new Produto();
-       // prod.setCategoria("brinquedo");
-        prod.setNome("buzz");
-        prod.setId(1L);
-        prod.setPreco(new BigDecimal(10.0));
-        prod.setQuantidadeEmEstoque(1);
-        prod.setDescricao("disney");
 
+        prod.setCategoriaID(1L);
+        prod.setId(2l);
+        prod.setNome("iPhone 11");
+        prod.setQuantidadeEmEstoque(20);
+        prod.setDescricao("celular de ultima geracao");
+        Categoria cat = new Categoria();
+
+        cat.setDescricao("eletronico");
+        cat.setId(1L);
+        cat.setNome("celulares");
+
+        prod.setCategoria(cat);
         return prod;
+    }
 
+    private Categoria categoria(){
+        Categoria cat = new Categoria();
+        cat.setDescricao("eletronicos");
+        cat.setId(1L);
+        cat.setNome("Eletronicos");
+        Produto prod = new Produto();
+        // List<Produto> produtolist = new ArrayList<>();
+        // produtolist.add(prod);
+        cat.setProdutos(new ArrayList<>());
+        return cat;
     }
 }
